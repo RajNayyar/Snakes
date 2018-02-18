246a36be9f2709d37078f23ff17912ba5009c384
Rajpreet Singh <rajpreetsingh0005@gmail.com>
2018-02-18 23:59:52 +0530

    Initial Commit

---
+++ snakes5/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  snakes5
+//
+//  Created by Rajpreet on 18/02/18.
+//  Copyright © 2018 Rajpreet. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
